1402cacebfb3d85fbfc7fc4908dcaadbc44eb54a
ivangaston <ivantruccolo@hotmail.com>
2023-07-06 17:12:15 +0200

    0.1

--- routes/infotres.js
+++ routes/infotres.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('Seccion info Uno');
+  res.send('Seccion info Tres');
 });
 
 module.exports = router;
